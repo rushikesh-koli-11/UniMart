28ea065e17549226486604745a7dcca3a4ccad00
rushikesh <rushikeshkoli87400@gmail.com>
2025-12-10 14:17:18 +0530

    bot link added to frontend chatbot.js api

--- frontend/src/api/chatbot.js
+++ frontend/src/api/chatbot.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const chatbotAPI = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://unimart-bot.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
